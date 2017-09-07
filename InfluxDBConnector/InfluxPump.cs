7aafd668a78ec9ebf1dd61650c5a272ec7ccb213
Andreas Ruhrmann <a.ruhrmann@copago.de>
2017-09-07 10:47:10 +0200

    Projektdateien hinzufügen.

---
+++ InfluxDBConnector/InfluxPump.cs
@@ -0,0 +1,38 @@
+﻿using InfluxDB.Collector;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
+
+namespace InfluxDBConnector
+{
+    internal class InfluxPump
+    {
+        public InfluxPump()
+        {
+            try
+            {
+                Metrics.Collector = new CollectorConfiguration()
+                .Tag.With("host", Environment.GetEnvironmentVariable("COMPUTERNAME"))
+                .Batch.AtInterval(TimeSpan.FromSeconds(30))
+                .WriteTo.InfluxDB("http://192.168.0.57:8086", "mssqltest", "copago", "Copago$2017")
+                .CreateCollector();
+                while (true)
+                {
+                    Process process = Process.GetCurrentProcess();
+                    Metrics.Write("cpu_time",
+                        new Dictionary<string, object>
+                        {
+                        { "value", process.TotalProcessorTime.TotalMilliseconds },
+                        { "user", process.UserProcessorTime.TotalMilliseconds }
+                        });
+                    Thread.Sleep(5000);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("3:" + $"{e.Data}: {e.Message}");
+            }
+        }
+    }
+}
